e38b04635e9131fe5654446abaf7728aee5d0397
Lagutin Daniil <ldv20u583@student.bmstu.ru>
2024-06-13 16:54:58 +0300

    Add local_registry interface

---
+++ lib/local_registry/registries/json_provider/src/lib.rs
@@ -0,0 +1,114 @@
+use std::path::Path;
+use std::io::Write;
+use serde_json as json;
+
+use logic::light::Light;
+use local_registry::{
+    Registry,
+    Error,
+    Result,
+};
+
+const DUMPS: &str = "dumps";
+const DEFULATS: &str = "defaults";
+
+struct JSONRegistry {
+    location: Path,
+}
+
+impl JSONRegistry {
+    fn ensure_path<P: AsRef<Path>>(self: &Self, path: P) -> Result<()> {
+        if self.location.exists() {
+            Ok(())
+        } else {
+            match std::fs::create_dir_all(path) {
+                Err(err) => return Error::internal(self.name(), Box::new(err)),
+                Ok(_) => Ok(()),
+            }
+        }
+    }
+
+    fn ensure_paths(self: &Self) -> Result<()> {
+        self.ensure_path(&self.location)?;
+        self.ensure_path(self.location.join(DUMPS))?;
+        self.ensure_path(self.location.join(DEFULATS))
+    }
+
+    fn check_light(self: &Self, light: &Light) -> Result<()> {
+        if "" == light.name {
+            Error::unnamed(self.name())
+        } else {
+            Ok(())
+        }
+    }
+
+    fn dump_to_file(self: &Self, subdir: &str, light: &Light) -> Result<()> {
+        self.check_light(light)?;
+        self.ensure_paths()?;
+
+        let path = self.location
+            .join(subdir)
+            .join(format!("{}.json", light.name));
+
+        match std::fs::File::create(path) {
+            Err(err) => Error::internal(self.name(), Box::new(err)),
+            Ok(file) => {
+                let mut writer = std::io::BufWriter::new(file);
+
+                if let Err(err) = json::to_writer(&mut writer, &light) {
+                    Error::internal(self.name(), Box::new(err))
+                } else if let Err(err) = writer.flush() {
+                    Error::internal(self.name(), Box::new(err))
+                } else {
+                    Ok(())
+                }
+            }
+        }
+    }
+}
+
+impl Registry for JSONRegistry {
+    fn name(self: &Self) -> &str {
+        "json"
+    }
+
+    fn list_defaults(self: &Self) -> Result<Vec<Light>> {
+        todo!()
+    }
+
+    fn list_dumps(self: &Self) -> Result<Vec<Light>> {
+        todo!()
+    }
+
+    fn load_default(self: &Self, name: &str) -> Result<Light> {
+        todo!()
+    }
+
+    fn load_dump(self: &Self, name: &str) -> Result<Light> {
+        todo!()
+    }
+
+    fn dump(self: &mut Self, light: &Light) -> Result<()> {
+        self.dump_to_file(DUMPS, light)
+    }
+
+    fn default(self: &mut Self, light: &Light) -> Result<()> {
+        self.dump_to_file(DEFULATS, light)
+    }
+
+    fn remove_dump(self: &mut Self, name: &str) -> Result<()> {
+        todo!()
+    }
+
+    fn remove_default(self: &mut Self, name: &str) -> Result<()> {
+        todo!()
+    }
+
+    fn rename_dump(self: &mut Self, old: &str, new: &str) -> Result<()> {
+        todo!()
+    }
+
+    fn rename_default(self: &mut Self, old: &str, new: &str) -> Result<()> {
+        todo!()
+    }
+}
--- lib/local_registry/src/lib.rs
+++ lib/local_registry/src/lib.rs
@@ -1,6 +1,103 @@
 
-trait Registry {
+use logic::light::Light;
+
+pub type Result<'a, T> = std::result::Result<T, Error<'a>>;
+
+pub trait Registry {
     fn name(self: &Self) -> &str;
-    // fn list
+    fn list_defaults(self: &Self) -> Result<Vec<Light>>;
+    fn list_dumps(self: &Self) -> Result<Vec<Light>>;
+    fn load_default(self: &Self, name: &str) -> Result<Light>;
+    fn load_dump(self: &Self, name: &str) -> Result<Light>;
+    fn dump(self: &mut Self, light: &Light) -> Result<()>;
+    fn default(self: &mut Self, light: &Light) -> Result<()>;
+    fn remove_dump(self: &mut Self, name: &str) -> Result<()>;
+    fn remove_default(self: &mut Self, name: &str) -> Result<()>;
+    fn rename_dump(self: &mut Self, old: &str, new: &str) -> Result<()>;
+    fn rename_default(self: &mut Self, old: &str, new: &str) -> Result<()>;
+}
+
+#[derive(Debug)]
+pub struct Error<'a> {
+    pub registry: &'a str,
+    pub etype: ErrorType<'a>,
+}
+
+#[derive(Debug)]
+pub enum ErrorType<'a> {
+    NotFound(String),
+    IncorrectLight(&'a Light),
+    Unnamed,
+    Internal(Box<dyn std::error::Error>),
+}
+
+impl<'a> Error<'a> {
+    pub fn not_found<T>(registry: &'a str, name: &str) -> Result<'a, T> {
+        Err(Self {
+            registry,
+            etype: ErrorType::NotFound(name.to_string()),
+        })
+    }
+
+    pub fn incorrect_light<T>(registry: &'a str, light: &'a Light) -> Result<'a, T> {
+        Err(Self {
+            registry,
+            etype: ErrorType::IncorrectLight(light),
+        })
+    }
+
+    pub fn internal<T>(registry: &'a str, err: Box<dyn std::error::Error>) -> Result<'a, T> {
+        Err(Self {
+            registry,
+            etype: ErrorType::Internal(err),
+        })
+    }
+
+    pub fn unnamed<T>(registry: &'a str) -> Result<'a, T> {
+        Err(Self {
+            registry,
+            etype: ErrorType::Unnamed,
+        })
+    }
+}
+
+impl<'a> From<Error<'a>> for ErrorType<'a> {
+    fn from(value: Error<'a>) -> Self {
+        value.etype
+    }
+}
+
+impl<'a> std::error::Error for Error<'a> {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match &self.etype {
+            ErrorType::Internal(err) => Some(err.as_ref()),
+            _ => None,
+        }
+    }
+}
+
+impl<'a> std::fmt::Display for ErrorType<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NotFound(name) => {
+                write!(f, "Can't find light named \"{}\"", name)
+            },
+            Self::IncorrectLight(light) => {
+                write!(f, "Incorrect light occured --- {:?}", light)
+            },
+            Self::Unnamed => {
+                write!(f, "Local registry can't manage unnamed lights")
+            }
+            Self::Internal(err) => {
+                write!(f, "Internal error occured\n{}", err)
+            },
+        }
+    }
+}
+
+impl<'a> std::fmt::Display for Error<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Registry \"{}\": {}", self.registry, self.etype)
+    }
 }
 
